6a479b935c2028440e2b0b01f004fbd1b0b71dae
Yogesh K <armsvcmode@gmail.com>
2022-06-25 11:32:01 +0530

    Create the homepage with a list of videos and add links to the videos. Image of next.js is used

---
+++ components/Video.js
@@ -0,0 +1,58 @@
+
+import timeago from 'lib/timeago'
+import Image from 'next/image'
+import Link from 'next/link'
+
+// print the video information.
+// For getting precisely 800x450 images in the list, we use Image from next.js,
+// instead of the regular img tag as it always gives original size
+export default function Video({ video }) {
+  return (
+    <div className=''>
+        <div className='px-5 pb-5'>
+            {video.thumbnail && (
+            <Link href={`/video/${video.id}`}>
+                <Image
+                    className='mb-2 cursor-pointer'
+                    src={video.thumbnail}
+                    width='800'
+                    height='450'
+                />
+            </Link>
+            )}
+            {/* video length in minutes and seconds: */}
+            <p className='text-white float-right relative -mt-11 mr-1 bg-black p-1'>
+                {Math.floor(video.length / 60)
+                    .toString()
+                    .padStart(2, '0')}
+                :{(video.length % 60).toString().padStart(2, '0')}
+            </p>
+
+        <div className='flex'>
+          {video.author.image && (
+            <img
+              className='w-10 h-10 mt-2 mr-2 rounded-full'
+              src={video.author.image}
+            />
+          )}
+                <div>
+                     <Link href={`/video/${video.id}`}>
+                        <a className='text-lg font-bold text-white'>{video.title}</a>
+                    </Link>
+                    <div className=''>
+                        <div className=''>
+                            <div className='text-gray-400'>
+                                <Link href={`/channel/${video.author.username}`}>
+                                    <a className='mr-2 underline'>{video.author.name}</a>
+                                </Link>
+                                    · {video.views} views ·{' '}
+                                    {timeago.format(new Date(video.createdAt))}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+        </div>
+        </div>
+      </div>
+  )
+}
--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
+// add the AWS S3 domains 
 module.exports = {
   reactStrictMode: true,
+
+  images: {
+    domains: [
+      'localhost',
+      'bootcamp-yogi.s3.amazonaws.com',
+      'bootcamp-yogi.s3.ap-south-1.amazonaws.com'
+    ],
+  },
 }
